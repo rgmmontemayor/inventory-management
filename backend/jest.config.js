84997b61c88a31fc17adf5551bf2f5ae3675908b
Sander Gervacio <sandergervacio295@gmail.com>
2026-02-25 14:55:05 +0800

    feat(products): implement create-product Lambda with DynamoDB ConditionExpression, Zod validation, and unit tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
-  testMatch: ['**/*.test.ts'],
+  roots: ['<rootDir>/lambda'],
+  testMatch: ["**/__tests__/**/*.test.ts"],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': ['ts-jest', {
+      isolatedModules: true,
+    }]
   },
   setupFilesAfterEnv: ['aws-cdk-lib/testhelpers/jest-autoclean'],
 };
